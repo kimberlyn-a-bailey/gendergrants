51720bb60580d79ea7ccbe277f6820581b6d00c2
Kimberlyn Bailey <kimberlyn.a.bailey@gmail.com>
2019-07-02 10:15:27 -0400

    Looking at NSF, NIH and Markowitz NSF data

--- test.R
+++ test.R
@@ -1,1 +1,15 @@
-print("Hi")
+########################Data prep
+
+#Read in data
+nsfData2 <- read.csv("/Users/user/Desktop/Gender Boldness/NSF_2006to2017_3.csv")
+
+#Remove NA columns
+#index <- map_lgl(nsfData, ~ all(is.na(.)))
+#nsfData <- nsfData[!index,]
+
+na.omit(nsfData2)
+
+boxplot(Award ~ Gender, data = nsfData, lwd = 2, ylab = 'NUMS')
+stripchart(Awarde ~ Gender, vertical = TRUE, data = nsfData, 
+           method = "jitter", add = TRUE, pch = 20, col = 'blue')
+
